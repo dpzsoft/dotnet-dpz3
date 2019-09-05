00dd5c760248156d0bfbed12ccfa2b22625992ce
inmount <inmount@163.com>
2019-09-05 22:35:18 +0800

    1.0.1909.2 全新的套件基于.Net Standard构建

---
+++ Net/HttpModules/HttpHeader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dpz3.Net.HttpModules {
+
+    /// <summary>
+    /// Http协议头部
+    /// </summary>
+    public class HttpHeader : dpz3.KeyValues<string> {
+
+        /// <summary>
+        /// 获取标准的字符串
+        /// </summary>
+        /// <returns></returns>
+        protected override string OnParseString() {
+
+            string res = "";
+
+            foreach (var item in this) {
+                //socket.Send(Encoding.ASCII.GetBytes($"{item.Key}:{item.Value}\r\n"));
+                res += $"{item.Key}:{item.Value}\r\n";
+            }
+
+            return res;
+        }
+
+    }
+}
