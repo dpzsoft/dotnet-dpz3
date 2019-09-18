84a16887b53e7c4667261ff7fc33575d94532804
inmount <inmount@163.com>
2019-09-18 22:36:40 +0800

    1.0.1909.15 修改HTML解析中的空格支持

--- Html/Parser.cs
+++ Html/Parser.cs
@@ -24,7 +24,7 @@ namespace dpz3.Html {
             res = res.Replace("&", "&amp;");//处理特殊输入
             //res = res.Replace("\r", "").Replace("\n", "&enter;");//处理换行
             res = res.Replace("\"", "&quot;");//处理双引号
-            //res = res.Replace(" ", "&nbsp;");//处理空格
+            res = res.Replace(" ", "&nbsp;");//处理空格
             res = res.Replace("<", "&lt;");//处理小于号
             res = res.Replace(">", "&gt;");//处理大于号
             res = res.Replace("'", "&apos;");//处理单引号
@@ -41,7 +41,7 @@ namespace dpz3.Html {
             //res = res.Replace("&enter;", "\r\n");//处理换行
             res = res.Replace("&quot;", "\"");//处理双引号
             res = res.Replace("&apos;", "'");//处理双引号
-            //res = res.Replace("&nbsp;", " ");//处理空格
+            res = res.Replace("&nbsp;", " ");//处理空格
             res = res.Replace("&lt;", "<");//处理小于号
             res = res.Replace("&gt;", ">");//处理大于号
             res = res.Replace("&amp;", "&");//处理特殊输入
