28377134290147764d08fe7b467ce5615a9757c0
inmount <inmount@163.com>
2020-04-19 11:24:15 +0800

    增加文件夹操作类

---
+++ dpz3/Folder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dpz3 {
+
+    /// <summary>
+    /// 文件夹操作
+    /// </summary>
+    public static class Folder {
+
+        /// <summary>
+        /// 创建文件夹
+        /// </summary>
+        /// <param name="path"></param>
+        public static void Create(string path) {
+            if (!System.IO.Directory.Exists(path)) System.IO.Directory.CreateDirectory(path);
+        }
+
+        /// <summary>
+        /// 获取所属子文件夹
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string[] GetDirectories(string path) {
+            return System.IO.Directory.GetDirectories(path);
+        }
+
+        /// <summary>
+        /// 获取所属文件
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="pattern"></param>
+        /// <returns></returns>
+        public static string[] GetFiles(string path, string pattern = null) {
+            if (pattern == null) {
+                return System.IO.Directory.GetFiles(path);
+            } else {
+                return System.IO.Directory.GetFiles(path, pattern);
+            }
+        }
+
+    }
+}
